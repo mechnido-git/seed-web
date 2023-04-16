5da1ad542f4e1942703a69ae7bc41dadb68d94e8
mishalp <mishalclever@gmail.com>
2023-04-16 17:55:09 +0530

    dahsboard update

--- src/pages/events/dashboard/Dashboard.js
+++ src/pages/events/dashboard/Dashboard.js
@@ -1,87 +1,269 @@
-import React, { useState } from 'react'
-import './dashboard.css'
+import React, { useState } from "react";
+import "./dashboard.css";
 
 function Dashboard() {
-    const [teamName, setTeamName] = useState('SAMPLE NAME')
-    const [teamMembers, setTeamMembers] = useState(5)
-    const [kartType, setKartType] = useState('')
-    const [collegeName, setCollegeName] = useState('Sample Name')
-    const [collegeAddress, setCollegeAddress] = useState('')
-    const [city, setCity] = useState('')
-    const [state, setState] = useState('')
-    const [pincode, setPincode] = useState('')
+  const [teamName, setTeamName] = useState("SAMPLE NAME");
+  const [teamMembers, setTeamMembers] = useState(5);
+  const [kartType, setKartType] = useState("");
+  const [collegeName, setCollegeName] = useState("Sample Name");
+  const [collegeAddress, setCollegeAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [pincode, setPincode] = useState("");
 
-    const onSumbitHandler = () => {
+  const facuality_model = {
+    img: "",
+    collegeId: "",
+    govId: "",
+    name: "",
+    designation: "",
+    department: "",
+    dob: "",
+    email: "",
+    mobile: "",
+  };
 
-    }
+  const [facualty, setFacualty] = useState([
+    { ...facuality_model },
+    { ...facuality_model },
+  ]);
+
+  const onSumbitHandler = () => {};
 
   return (
-    <div className='dashboard'>
-        <h2>Dashboard</h2>
-        <div className="team-logo">
-            <div className="logo"></div>
-            <button>Edit</button>
-        </div>
-    <div className="form-div">
-        
-    <form onSubmit={onSumbitHandler}>
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <div className="team-logo">
+        <div className="logo"></div>
+        <button>Edit</button>
+      </div>
+      <div className="form-div">
+        <form onSubmit={onSumbitHandler}>
+          <div className="basic-info">
             <div className="input-div">
-            <label htmlFor="team-name">Team Name</label> :
-            <input value={teamName} type="text" name='teamName' required onChange={(e)=>setTeamName(e.target.value.toUpperCase())}/>
+              <label htmlFor="team-name">Team Name</label> :
+              <input
+                value={teamName}
+                type="text"
+                name="teamName"
+                required
+                onChange={(e) => setTeamName(e.target.value.toUpperCase())}
+              />
             </div>
 
             <div className="input-div">
-            <label htmlFor="team-members">No of Team Members</label> :
-            <input value={teamMembers} type="number" min={3} max={25} name='teamMembers' required onChange={(e)=>setTeamMembers(e.target.value)}/>
+              <label htmlFor="team-members">No of Team Members</label> :
+              <input
+                value={teamMembers}
+                type="number"
+                min={3}
+                max={25}
+                name="teamMembers"
+                required
+                onChange={(e) => setTeamMembers(e.target.value)}
+              />
             </div>
 
             <div className="input-div">
-            <label htmlFor="Kart Type">Kart Type</label> :
-            <select name="kartType" id="kartType" value={kartType} onChange={(e)=>setKartType(e.target.value)}>
+              <label htmlFor="Kart Type">Kart Type</label> :
+              <select
+                name="kartType"
+                id="kartType"
+                value={kartType}
+                onChange={(e) => setKartType(e.target.value)}
+              >
                 <option value="gokart">Gokart</option>
                 <option value="ekart">E-Kart</option>
-            </select>
+              </select>
             </div>
 
             <div className="input-div">
-            <label htmlFor="college-name">College Name</label> :
-            <input value={collegeName} type="text" name='collegeName' required onChange={(e)=>setCollegeName(e.target.value)}/>
+              <label htmlFor="college-name">College Name</label> :
+              <input
+                value={collegeName}
+                type="text"
+                name="collegeName"
+                required
+                onChange={(e) => setCollegeName(e.target.value)}
+              />
             </div>
 
             <div className="input-div">
-            <label htmlFor="college-adress">College Address</label> :
-            <input value={collegeAddress} type="text" name='collegeAddress' required onChange={(e)=>setCollegeAddress(e.target.value)}/>
+              <label htmlFor="college-adress">College Address</label> :
+              <input
+                value={collegeAddress}
+                type="text"
+                name="collegeAddress"
+                required
+                onChange={(e) => setCollegeAddress(e.target.value)}
+              />
             </div>
 
             <div className="input-div">
-            <label htmlFor="city">City</label> :
-            <input value={city} type="text" name='city' required onChange={(e)=>setCity(e.target.value)}/>
+              <label htmlFor="city">City</label> :
+              <input
+                value={city}
+                type="text"
+                name="city"
+                required
+                onChange={(e) => setCity(e.target.value)}
+              />
             </div>
 
             <div className="input-div">
-            <label htmlFor="state">State</label> :
-            <input value={state} type="text" name='state' required onChange={(e)=>setState(e.target.value)}/>
-            </div>
-            
-            <div className="input-div">
-            <label htmlFor="pincode">Pincode</label> :
-            <input value={pincode} type="number" name='pincode' required onChange={(e)=>setPincode(e.target.value)}/>
+              <label htmlFor="state">State</label> :
+              <input
+                value={state}
+                type="text"
+                name="state"
+                required
+                onChange={(e) => setState(e.target.value)}
+              />
             </div>
 
-            <hr />
-
             <div className="input-div">
+              <label htmlFor="pincode">Pincode</label> :
+              <input
+                value={pincode}
+                type="number"
+                name="pincode"
+                required
+                onChange={(e) => setPincode(e.target.value)}
+              />
+            </div>
+          <hr />
+          <div className="input-div">
             <label htmlFor="pincode">Facualty</label> :
-            <div style={{display: 'flex', alignItems: 'center', gap: '5px', textAlign: 'center'}} >
-            <input type="checkbox" name='fac' style={{height: '100%', padding: '10px',}} required onChange={(e)=>setPincode(e.target.value)}/>  one
-            <input type="checkbox" name='fac' required onChange={(e)=>setPincode(e.target.value)}/>  two
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "5px",
+                textAlign: "center",
+              }}
+            >
+              <input
+                type="checkbox"
+                name="fac"
+                style={{ height: "100%", padding: "10px" }}
+              />
+              one
+              <input
+                type="checkbox"
+                name="fac"
+              />
+              two
             </div>
-            </div>
-        </form>
-    </div>
+          </div>
 
+          </div>
+
+
+
+          <div className="fac-div">
+            {facualty.map((item, index) => {
+              return (
+                <div className="fac">
+                    <h3>Facualty Advisor {index+1}</h3>
+                  <div className="input-div">
+                    <label htmlFor="photo">Passport Image</label>:
+                    <div className="div"></div>
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">College Id</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Government Id</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Name</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Designation</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Department</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Date of Birth</label>
+                    :
+                    <input type="date" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Email ID</label>
+                    :
+                    <input type="email" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Mobile</label>
+                    :
+                    <input type="number" />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <hr />
+            <h3 style={{margin: "1rem"}}>Team Details</h3>
+          <div className="fac-div">
+            {facualty.map((item, index) => {
+              return (
+                <div className="fac">
+                    <h3>Member {index+1}</h3>
+                  <div className="input-div">
+                    <label htmlFor="photo">Passport Image</label>:
+                    <div className="div"></div>
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">College Id</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Year of Study</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">{index == 0 && "Captain "}Name</label>
+                    :
+                    <input type="text" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Date of Birth</label>
+                    :
+                    <input type="date" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Email ID</label>
+                    :
+                    <input type="email" />
+                  </div>
+                  <div className="input-div">
+                    <label htmlFor="college">Mobile</label>
+                    :
+                    <input type="number" />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <hr />
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
