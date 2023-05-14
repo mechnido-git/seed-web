89a124171585879d54ef374133fbddbb1d6d0de6
mishalp <mishalclever@gmail.com>
2023-05-14 11:51:56 +0530

    details

--- src/pages/courses/CourseDetails.js
+++ src/pages/courses/CourseDetails.js
@@ -3,8 +3,10 @@ import { useLocation, useParams } from "react-router-dom";
 import { StoreContext } from "../../store/StoreContext";
 import img from "../../images/courses.jpg";
 import side from "../../images/side.jpg";
+import cert from "../../images/cert.jpg"
 
 import "./details.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 function CourseDetails() {
   const { index } = useParams();
@@ -42,13 +44,20 @@ function CourseDetails() {
                 ))}
               </ul>
               <h3>
-                <span class="material-symbols-outlined">currency_rupee</span>Fee Structure
+                <span class="material-symbols-outlined">currency_rupee</span>Fee
+                Structure
               </h3>
               <ul>
                 {data.fee.map((item, i) => (
-                  <li className="fee-li" key={i}><span>{item.type}</span>: <span>{item.price}₹</span> </li>
+                  <li className="fee-li" key={i}>
+                    <span>{item.type}</span>: <span>{item.price}₹</span>{" "}
+                  </li>
                 ))}
               </ul>
+              <h3>
+                <span class="material-symbols-outlined">workspace_premium</span>
+                Course certificate
+              </h3>
             </div>
           </div>
         </div>
@@ -56,31 +65,39 @@ function CourseDetails() {
       <div className="about">
         <h2>About the Course</h2>
         <div className="details">
-            <div className="left">
-            {data.description_L.map((item, i)=><p key={i}>{item}</p>)}
-            </div>
-            <div className="right">
-                <img src={side} alt="" />
-            </div>
+          <div className="left">
+            {data.description_L.map((item, i) => (
+              <p key={i}>{item}</p>
+            ))}
+          </div>
+          <div className="right">
+            <img src={side} alt="" />
+          </div>
         </div>
       </div>
       <div className="explore">
         <h2>Explore the course</h2>
         <div className="content">
-            <div className="topics">
-            {data.schedule.map((item, i)=><div key={i} className='main'>
+          <div className="topics">
+            {data.schedule.map((item, i) => (
+              <div key={i} className="main">
                 <h3>{item.name}</h3>
                 <ol>
-                {item.topics.map((item, i)=><li key={i} className='topic'>
-                   <h4 onClick={showSubs} >{item.name}</h4>
-                   <ul>
-                   {item.SUB.map((item, i)=><li key={i}>{item}</li>)}
-                   </ul>
-                </li>)}
+                  {item.topics.map((item, i) => (
+                    <li key={i} className="topic">
+                      <h4 onClick={showSubs}>{item.name}</h4>
+                      <ul>
+                        {item.SUB.map((item, i) => (
+                          <li key={i}>{item}</li>
+                        ))}
+                      </ul>
+                    </li>
+                  ))}
                 </ol>
-            </div>)}
-            </div>
-            <div className="card">
+              </div>
+            ))}
+          </div>
+          <div className="card">
             <img src={img} alt="" />
             <button>Enroll Now</button>
             <hr />
@@ -94,16 +111,69 @@ function CourseDetails() {
                 ))}
               </ul>
               <h3>
-                <span class="material-symbols-outlined">currency_rupee</span>Fee Structure
+                <span class="material-symbols-outlined">currency_rupee</span>Fee
+                Structure
               </h3>
               <ul>
                 {data.fee.map((item, i) => (
-                  <li className="fee-li" key={i}><span>{item.type}</span>: <span>{item.price}₹</span> </li>
+                  <li className="fee-li" key={i}>
+                    <span>{item.type}</span>: <span>{item.price}₹</span>{" "}
+                  </li>
                 ))}
               </ul>
+              <h3>
+                <span class="material-symbols-outlined">workspace_premium</span>
+                Course certificate
+              </h3>
             </div>
           </div>
         </div>
+      </div>
+      <div className="certificate">
+        <h2>Certificate <span class="material-symbols-outlined">workspace_premium</span></h2>
+        <h3>{data.certificate.title}</h3>
+        <div className="content">
+            <ul>{data.certificate.sub.map((item, i)=><li key={i}>{item}</li>)}</ul>
+        </div>
+      </div>
+      <div className="feedback">
+        <h2>Feedbacks</h2>
+        <Splide
+          tag="section"
+          aria-labelledby="My Favorite Images"
+          options={{
+            speed: 1000,
+            autoplay: true,
+            interval: 3400,
+            arrows: false,
+            pauseOnHover: false,
+            type: "loop",
+            pauseOnFocus: true,
+            keyboard: true,
+            gap: ".5rem",
+            width: "100%",
+            perPage:
+              window.innerWidth <= 426
+                ? 1.2
+                : window.innerWidth <= 768
+                ? 1.5
+                : window.innerWidth <= 1024
+                ? 2
+                : 3,
+            perMove: 1,
+            pagination: false,
+          }}
+        >
+          {data.feedback.map((item, i) => (
+            <SplideSlide key={i}>
+              <div className="info">
+                <img src={item.dp} alt="" />
+                <h4>{item.name}</h4>
+              </div>
+              <p>"{item.msg}"</p>
+            </SplideSlide>
+          ))}
+        </Splide>
       </div>
     </div>
   );
--- src/store/StoreContext.js
+++ src/store/StoreContext.js
@@ -1,145 +1,218 @@
 import { getAuth } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/config";
+import p1 from "../images/p1.jpg";
+import p2 from "../images/p2.jpg";
+import p3 from "../images/p3.jpg";
+import p4 from "../images/p4.jpg";
+import p5 from "../images/p5.jpg";
+import p6 from "../images/p6.jpg";
+import p7 from "../images/p7.jpg";
 
-export const  StoreContext = createContext(null);
+export const StoreContext = createContext(null);
 
-const Store = ({children}) => {
-    const [userId, setUserId] = useState(null);
-    const [userName, setUserName] = useState(null);
-    const [user, setUser] = useState(null);
-    const [change, setChange] = useState(true);
-    const courseList = [
+const Store = ({ children }) => {
+  const [userId, setUserId] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [user, setUser] = useState(null);
+  const [change, setChange] = useState(true);
+  const courseList = [
+    {
+      name: "Product Development Program",
+      thumbnail: "../../images/courses.jpg",
+      description:
+        "This Program offers students to get clear insights of product development concepts by hands-on training which will be handled by Mechnido's faculty and industrial experts",
+      description_L: [
+        "This Program offers students to get clear insights of product development concepts by hands-on training which will be handled by Mechnido's faculty and industrial experts",
+        "In 18+ hours sessions, students will get an overview of the concepts and potential opportunities in the field. Moreover, students will get certificates from Mechnido.",
+        "This program gives a general outlook on how to identify real time problems and forming solutions, by systematic approaches like applying , Design Thinking , Reverse Engineering, root cause analysis, DFMEA and other relevant tools.",
+        "The topics are explained using product development strategy. Moreover, in this session students get brief explanation of new product trends and opportunities.",
+      ],
+      duration: ["Day 01 - 06 Hrs", "Day 02 - 06 Hrs", "Day 03 - 06 Hrs"],
+      feedback: [
         {
-          name: "Product Development Program",
-          thumbnail: "../../images/courses.jpg",
-          description: "This Program offers students to get clear insights of product development concepts by hands-on training which will be handled by Mechnido's faculty and industrial experts",
-          description_L: [
-            "This Program offers students to get clear insights of product development concepts by hands-on training which will be handled by Mechnido's faculty and industrial experts",
-            "In 18+ hours sessions, students will get an overview of the concepts and potential opportunities in the field. Moreover, students will get certificates from Mechnido.",
-            "This program gives a general outlook on how to identify real time problems and forming solutions, by systematic approaches like applying , Design Thinking , Reverse Engineering, root cause analysis, DFMEA and other relevant tools.",
-            "The topics are explained using product development strategy. Moreover, in this session students get brief explanation of new product trends and opportunities.",
-          ],
-          duration: ["Day 01 - 06 Hrs", "Day 02 - 06 Hrs", "Day 03 - 06 Hrs"],
-          fee: [
+          name: "PRAVEEN D [STUDENT]",
+          msg: "I adore how natural it is for me to be open and connect with the other participants in this programme. The steps in this class were designed to put us in a position where we could confidently continue tapping after it ended.",
+          dp: p1,
+        },
+        {
+          name: "TAMIL DHARANI [STUDENT]",
+          msg: 'I will say it as "Two extremely useful and educational days". The Product Development Program was great, especially because everyone there was so nice.',
+          dp: p2,
+        },
+        {
+          name: "JEYANTH P [STUDENT]",
+          msg: "I found the workshop to be very informative. My hands-on skills were specifically enhanced by the session. In both my personal and professional life, the skills will be priceless.",
+          dp: p3,
+        },
+        {
+          name: "YASHITH M V [STUDENT]",
+          msg: "The content was comprehensive, excellent, very thorough, and suitable for people of all skill levels because we had the chance to implement what we learned immediately. such a great session, really comprehensive.",
+          dp: p4,
+        },
+      ],
+      certificate: {
+        title: "Requirements for Certification",
+        sub: [
+          "An individual enrolled in the program should complete the training within the program's time limit.",
+          "To acquire the certification, you must complete all the course's assignments, tests, case studies, projects, and other criteria.",
+          "To acquire certification, the student must show up to every session that the instructor provides.",
+        ],
+      },
+      fee: [
+        {
+          type: "One Day",
+          price: 200,
+        },
+        {
+          type: "Two Days",
+          price: 350,
+        },
+        {
+          type: "Three Days",
+          price: 450,
+        },
+      ],
+      schedule: [
+        {
+          name: "Day 01",
+          topics: [
             {
-              type: "One Day",
-              price: 200
-            },
-            {
-              type: "Two Days",
-              price: 350
-            },
-            {
-              type: "Three Days",
-              price: 450
-            },
-          ],
-          schedule: [
-            {
-              name: "Day 01",
-              topics: [
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
+              name: "PRODUCT DEVELOPMENT",
+              SUB: [
+                "Introduction to Product Development",
+                "Product management Tool - Road mapping",
+                "Net Present Value (NPV)",
+                "Introduction to Pattern Rights - Utility, Design, and Plant",
+                "Case Study",
               ],
             },
             {
-              name: "Day 02",
-              topics: [
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
-                {
-                  name: "PRODUCT DEVELOPMENT",
-                  SUB: [
-                    "Introduction to Product Development",
-                    "Product management Tool - Road mapping",
-                    "Net Present Value (NPV)",
-                    "Introduction to Pattern Rights - Utility, Design, and Plant",
-                    "Case Study",
-                  ],
-                },
+              name: "REVERSE ENGINEERING",
+              SUB: [
+                "How to develop a existing Product",
+                "Root Cause Analysis",
+                "Net Present Value (NPV)",
+                "FMEA",
+                "Case Study",
+              ],
+            },
+            {
+              name: "DESIGN THINKING",
+              SUB: [
+                "How to Develop a innovating Product",
+                "What — Definition of Design Thinking",
+                "Process of DT",
+                "Case Study",
+              ],
+            },
+            {
+              name: "BOI (Business Opportunities Identification)",
+              SUB: [
+                "Forming business model & Market Strategy",
+                "Fund pitching and launching the product or service",
+                "Consumer segmentation",
+                "Purchase situation analysis",
+                "Direct competitor analysis",
+                "Indirect competitor analysis",
+                "Case Study",
               ],
             },
           ],
         },
-      ];
-      
+        {
+          name: "Day 02",
+          topics: [
+            {
+              name: "PRODUCT DEVELOPMENT",
+              SUB: [
+                "Kano Model",
+                "TRIZ (theory of inventive problem solving)",
+                "Scoring methods for technology and project prioritisation",
+                "How to Apply Pattern Rights - Utility, Design, and Plant",
+                "Case Study",
+              ],
+            },
+            {
+              name: "REVERSE ENGINEERING",
+              SUB: [
+                "Process and Implementation",
+                "Requirements",
+                "Use Mathematics and Computational Thinking",
+                "Problem Analyzing Too",
+              ],
+            },
+            {
+              name: "DESIGN THINKING",
+              SUB: [
+                "How — The Process in-depth",
+                "Flexibility — Adapt to Fit Your Needs",
+                "Why — The Advantage",
+                "Scalability — Think Bigger",
+                "Case Study",
+              ],
+            },
+            {
+              name: "BOI (Business Opportunities Identification)",
+              SUB: [
+                "How to Get Fund from Govt & Non Govt Organization",
+                "Foreign market analysis",
+                "Environmental analysis",
+                "Market Strategy",
+                "Case Study",
+              ],
+            },
+          ],
+        },
+        {
+          name: "Day 03",
+          topics: [
+            {
+              name: "PRODUCT DEVELOPMENT",
+              SUB: [
+                "Team up",
+                "Case Study By Students",
+                "Developing a Product by students",
+                "Creating Structurer",
+              ],
+            },
+            {
+              name: "BOI (Business Opportunities Identification)",
+              SUB: [
+                "Company Registration",
+                "Certification",
+                "Forming business model for there product",
+                "Pitching model for Joint venture / capital Venture",
+                "Case Study",
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
-    useEffect(()=>{console.log(user);}, [user])
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
 
-    return(
-
-    <StoreContext.Provider value={{userId, setUserId, userName, setUserName, user, setUser, change, setChange, courseList}}>
-        {children}
+  return (
+    <StoreContext.Provider
+      value={{
+        userId,
+        setUserId,
+        userName,
+        setUserName,
+        user,
+        setUser,
+        change,
+        setChange,
+        courseList,
+      }}
+    >
+      {children}
     </StoreContext.Provider>
-    )
-        
-}
+  );
+};
 
-export default Store
+export default Store;
