0d44e0c8c5e67315adf23722c2bbcfd8a315d42f
Ansh2030 <anshsrivastava360@gmail.com>
2023-07-10 16:53:52 +0530

    The first two sections of events page completed

--- src/pages/events/mainSection/MainSection.js
+++ src/pages/events/mainSection/MainSection.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import "./mainsectionstyle.css";
+import tnkc from "../../../images/tnkcimage.png";
+
 function MainSection({event}) {
   return (
+    <>
+    <div className='mainsection'>
+      
     <div className="title"> 
-      <h1>TNKC</h1>
-      <h2>TAMILNADU CARTING CHAMPIONSHIP</h2>
+      <h1><span style={{color:"var(--primary)"}}>TN</span>KC</h1>
+      <h2 className='subtitle'> {event.subtitle}</h2>
+      <p>{event.about.description}</p>
+      <div className='btn'>Explore</div>
+    </div>
+
+    <div className='titleimage'>
+      <img className='image' src= {tnkc} alt="tnkc image"></img>
+    </div>
 
     </div>
+
+   
+    </>
   )
 }
 
