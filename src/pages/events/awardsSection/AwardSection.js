b62ded786598140bbb333d985261d6cf6305d71c
Ansh2030 <anshsrivastava360@gmail.com>
2023-07-11 17:49:51 +0530

    schedule section and award sections on event page completed

--- src/pages/events/awardsSection/AwardSection.js
+++ src/pages/events/awardsSection/AwardSection.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
-function AwardSection() {
+import React, { useState } from 'react'
+import AwardItem from './AwardItem';
+import trophyy from "../../../images/trophy2.png";
+function AwardSection({event}) {
+  const dt = event.awards.array[0].data;
+  const pt= event.awards.array[1].data;
   return (
-    <div>AwardSection</div>
+    <>
+    <h2>Awards & Prizes</h2>
+    <div className='titletrophy'>
+   <h3 className='awtitle'>{event.awards.array[0].title}</h3>
+    <img className='tphy' src={trophyy} alt="this is trophy "/>
+   </div>
+    
+    <div className='grid-container'>
+      {
+        dt.map((element)=>{
+          return <>
+           <AwardItem con={element.h1} pr={element.p} inr={element.money}/>
+          </>
+        })
+      }
+    </div>
+
+   
+    <div className='titletrophy'>
+   <h3 className='awtitle'>{event.awards.array[1].title}</h3>
+    <img className='tphy' src={trophyy} alt="this is trophy "/>
+   </div>
+   
+
+    <div className='grid-container'>
+      {
+        pt.map((element)=>{
+          return <>
+           <AwardItem con={element.h1} pr={element.p} inr={element.money}/>
+          </>
+        })
+      }
+    </div>
+    </>
   )
 }
 
-export default AwardSection
+export default AwardSection;
