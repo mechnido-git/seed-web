d115ea2d648728927d9aad38d0e4bedf759bbc7e
Ansh2030 <anshsrivastava360@gmail.com>
2023-07-27 13:14:58 +0530

    3 bugs in darkmode fixed

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,14 +3,20 @@ import ScaleLoader from "react-spinners/ScaleLoader";
 import ClipLoader from "react-spinners/ClipLoader";
 import Quote from "../components/Quote";
 import logo from "../images/seed_logo/Seed Joined.svg";
+import logowhite from "../images/seed_white.png";
+import { useContext } from "react";
 import "./quote.css";
+import { StoreContext } from "../store/StoreContext";
+
+
 function Spinner({ loading, other, normal }) {
+  const {check}= useContext(StoreContext);
   return (
     <div className={`spinner ${other}`}>
       <div className="loading-container">
         {!normal && (
           <>
-            <img src={logo} alt=" company logo" />
+            <img src={check ? logowhite: logo} alt=" company logo" />
             <Quote />
           </>
         )}
