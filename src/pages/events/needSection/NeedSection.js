56d5001b941e034629d5c6866065d187134f69bb
Ansh2030 <anshsrivastava360@gmail.com>
2023-08-01 18:52:38 +0530

    Refactored need section

--- src/pages/events/needSection/NeedSection.js
+++ src/pages/events/needSection/NeedSection.js
@@ -11,52 +11,25 @@ function NeedSection({event}) {
   useEffect(()=>{
     console.log(" check value is ",check);
   },[theme]);
+
+  const sibling = event.why.sibling;
+ 
   return (
     <div>
        <div className='whysection' id='features'> 
     <div>
       <h2 className='whyh1'>Why <span style={{color:"var(--primary)"}}>TN</span>KC 2023?</h2>
       <div class="grid-container">
-  <div class="grid-item">
-   <img className='devimg' src={ check ? tickwhite : tick } alt="image1"/>
-   
-    <h5 className='devtitle'>{event.why.sibling[0].title}</h5>
-    <p className='devdes'>{event.why.sibling[0].description}</p>
-  </div>
-  <div class="grid-item">
-  <img className='devimg' src={ check ? tickwhite : tick } alt="image2"/>
-    <h5 className='devtitle'>{event.why.sibling[1].title}</h5>
-    <p className='devdes'>{event.why.sibling[1].description}</p>
-  </div>
-  <div class="grid-item">
-  <img className='devimg' src={ check ? tickwhite : tick } alt="image3"/>
-    <h5 className='devtitle'>{event.why.sibling[2].title}</h5>
-    <p className='devdes'>{event.why.sibling[2].description}</p></div>  
-  <div class="grid-item">
-  <img className='devimg' src={ check ? tickwhite : tick } alt="image4"/>
-    <h5 className='devtitle'>{event.why.sibling[3].title}</h5>
-    <p className='devdes'>{event.why.sibling[3].description}</p>
-  </div>
-  <div class="grid-item">
-  <img className='devimg' src={ check ? tickwhite : tick } alt="image5"/>
-    <h5 className='devtitle'>{event.why.sibling[4].title}</h5>
-    <p className='devdes'>{event.why.sibling[4].description}</p>
-  </div>
-  <div class="grid-item">
-  <img className='devimg' src={ check ? tickwhite : tick } alt="image6"/>
-    <h5 className='devtitle'>{event.why.sibling[5].title}</h5>
-    <p className='devdes'>{event.why.sibling[5].description}</p></div>  
-  <div class="grid-item">
-  <img className='devimg' src={ check ? tickwhite : tick } alt="image7"/>
-    <h5 className='devtitle'>{event.why.sibling[6].title}</h5>
-    <p className='devdes'>{event.why.sibling[6].description}</p>
-  </div>
-  <div class="grid-item">
-  <img className='devimg' src={ check ? tickwhite : tick } alt="image8"/>
-    <h5 className='devtitle'>{event.why.sibling[7].title}</h5>
-    <p className='devdes'>{event.why.sibling[7].description}</p>
-  </div>
-   
+    {
+      sibling.map((element)=>{
+        return  <div class="grid-item">
+        <img className='devimg' src={ check ? tickwhite : tick } alt="image"/>
+          <h5 className='devtitle'>{element.title}</h5>
+          <p className='devdes'>{element.description}</p>
+        </div>
+      })
+    }
+    
 </div>
 
     </div>
