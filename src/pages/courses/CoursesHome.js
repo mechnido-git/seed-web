afc9c7f77cec565bec622800a4d9261c47ac6b5e
mishalp <mail4mishal@gmail.com>
2023-08-19 19:41:50 +0530

    button update

--- src/pages/courses/CoursesHome.js
+++ src/pages/courses/CoursesHome.js
@@ -217,7 +217,7 @@ function CoursesHome() {
                 aria-labelledby="My Favorite Images"
                 options={{
                   speed: 1500,
-                  autoplay: true,
+                  autoplay: false,
                   interval: 3400,
                   pauseOnHover: true,
                   type: "loop",
